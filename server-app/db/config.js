35b1e14b07b51f719f594fbdaea009b255487ac5
yousif-ahmed <yousif.lasheen60@gmail.com>
2022-12-08 00:27:36 +0200

    feat : config database with sequelize orm

--- server-app/db/config.js
+++ server-app/db/config.js
@@ -1,35 +1,21 @@
-const util =  require ( 'util' );
-var mysql =  require ( 'mysql' );
+const Sequelize = require("sequelize");
 
-const db = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: 'password',
-  database: 'test'
-});
+const db = new Sequelize(
+  process.env.DATABASE,
+  process.env.USER,
+  process.env.PASSWORD,
+   {
+     host: process.env.HOST,
+     dialect: 'mysql'
+   }
+ );
 
-db.getConnection((err, connection) => {
-  if (err) {
-    if (err.code === 'PROTOCOL_CONNECTION_LOST') {
-      console.error('Database connection was closed.')
-    }
-    if (err.code === 'ER_CON_COUNT_ERROR') {
-      console.error('Database has too many connections.')
-    }
-    if (err.code === 'ECONNREFUSED') {
-      console.error('Database connection was refused.')
-    }
-  }else 
-     console.log("DB connected to my sql");
-
-
-  if (connection) connection.release()
-
-  return
-});
-
-db.query = util.promisify(db.query);
-module.exports = db;
+ db.authenticate().then(() => {
+   console.log('Connection has been established successfully.');
+   }).catch(err => {
+    console.error('Unable to connect to the database:', err);
+  }); 
+  
 
 
 
--- server-app/index.js
+++ server-app/index.js
@@ -1,13 +1,15 @@
 const express = require('express');
 const path = require('path');
 const bodyParser = require('body-parser');
-const routes = require('./routes');
+// const routes = require('./routes');
 const app = express();
+const port = process.env.PORT || 8080;
 
-const port = process.env.PORT || 3000;
+require("dotenv").config();
+require('./db/config');
 
 app.use(express.json());
-app.use(routes);
+// app.use(routes);
 app.use(express.static("files"));
 
 // list all routes 
