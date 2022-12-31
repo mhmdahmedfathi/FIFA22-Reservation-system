5a4f47d7f2318d91516f1cc6d502aedfe864b720
Mohamed Ahmed <mohamed.sayed001@eng-st.cu.edu.eg>
2022-12-31 15:30:14 +0200

    fix login

--- client-app/src/pages/Helpers/auth.js
+++ client-app/src/pages/Helpers/auth.js
@@ -1,38 +1,40 @@
-import { authAction } from '../../StateManagment/Auth/reducer';
-import AxiosConfiged from '../../axiosConfig';
+import { authAction } from "../../StateManagment/Auth/reducer";
+import AxiosConfiged from "../../axiosConfig";
 
 const signup = async (SignupData) => {
-    try {
-        const response = await AxiosConfiged.post(`/register`, SignupData);
-        return response.data;
-    } catch (error) {
-        return error.message;
-    }
-}
+  try {
+    const response = await AxiosConfiged.post(`/register`, SignupData);
+    return response.data;
+  } catch (error) {
+    return error.message;
+  }
+};
 
 const login = async (LoginData) => {
-    try {
-
-        const response = await AxiosConfiged.post(`/login`, LoginData);
-        console.log(response.data);
-        localStorage.setItem('token', response.data.token);
-        authAction.loggedIn({ username: response.data.user.username, role: response.data.user.role });
-        return response.data;
-    } catch (error) {
-        return error.message;
+  try {
+    const response = await AxiosConfiged.post(`/login`, LoginData);
+    if (response.data.token) {
+      localStorage.setItem("token", response.data.token);
+      authAction.loggedIn({
+        username: response.data.user.username,
+        role: response.data.user.role,
+      });
     }
-}
+    return response;
+  } catch (error) {
+    return error.message;
+  }
+};
 
 const logout = async () => {
-    try {
-        // const response = await AxiosConfiged.post(`/logout`);
-        localStorage.removeItem('token');
-        authAction.loggedOut();
-        return true;
-    } catch (error) {
-        return error.message;
-    }
-}
-
+  try {
+    // const response = await AxiosConfiged.post(`/logout`);
+    localStorage.removeItem("token");
+    authAction.loggedOut();
+    return true;
+  } catch (error) {
+    return error.message;
+  }
+};
 
 export { signup, login, logout };
--- client-app/src/pages/Login.jsx
+++ client-app/src/pages/Login.jsx
@@ -1,106 +1,106 @@
-import React, { useState } from 'react';
-import { Link, Redirect } from 'react-router-dom';
-import useInput from '../hooks/useInput';
-import { login } from './Helpers/auth';
+import React, { useState } from "react";
+import { Link, Redirect } from "react-router-dom";
+import useInput from "../hooks/useInput";
+import { login } from "./Helpers/auth";
 
 function Login() {
-    const [success, setSuccess] = useState(false);
-    const [loading, setLoading] = useState(false);
-    const {
-        value: name,
-        error: errorName,
-        handleChange: changeName,
-    } = useInput((value) => value.length < 3);
+  const [success, setSuccess] = useState(false);
+  const [error, seterror] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const {
+    value: name,
+    error: errorName,
+    handleChange: changeName,
+  } = useInput((value) => value.length < 3);
 
-    const {
-        value: password,
-        error: errorPassword,
-        handleChange: changePassword,
-    } = useInput((value) => value.length < 3);
+  const {
+    value: password,
+    error: errorPassword,
+    handleChange: changePassword,
+  } = useInput((value) => value.length < 3);
 
-
-    const handleSubmit = async (e) => {
-        e.preventDefault();
-        setLoading(true);
-        const LoginData = {
-            username: name,
-            password,
-        };
-        const res = await login(LoginData);
-        if (!res.error) {
-            setSuccess(true);
-        } else {
-            console.log(res.error);
-        }
-        setLoading(false);
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    setLoading(true);
+    const LoginData = {
+      username: name,
+      password,
     };
-
-    if (success) {
-        return <Redirect to="/admin" />;
+    const res = await login(LoginData);
+    if (res.data) {
+      setSuccess(true);
+    } else {
+      seterror(res);
     }
+    setLoading(false);
+  };
 
+  if (success) {
+    return <Redirect to="/admin" />;
+  }
 
-    return (
-        <div
-            className="d-flex justify-content-center align-items-center"
-            style={{ minHeight: '100vh' }}
-        >
-            <Link
-                to="/"
-                className="btn btn-secondary position-fixed top-0 start-0 m-2"
-            >
-                Home
-            </Link>
-            <form onSubmit={handleSubmit} className="p-4 bg-dark text-light rounded">
-                <h2 className="text-center mb-3">Login</h2>
-                {(errorName || errorPassword) && (
-                    <div className="alert alert-danger p-2 mb-3" role="alert">
-                        {errorName ? "Make sure you entered the username correct" :
-                            "Make sure you entered the password correct"}
-                    </div>
-                )}
-                <div className="mb-3">
-                    <label htmlFor="username" className="form-label mb-1">
-                        Username
-                    </label>
-                    <input
-                        type="text"
-                        className="form-control"
-                        name="username"
-                        id="username"
-                        placeholder="Username"
-                        value={name}
-                        onChange={changeName}
-                        required
-                        autoFocus
-                    />
-                </div>
-                <div className="mb-3">
-                    <label htmlFor="password" className="form-label mb-1">
-                        Password
-                    </label>
-                    <input
-                        type="password"
-                        className="form-control"
-                        name="password"
-                        id="password"
-                        placeholder="Password"
-                        value={password}
-                        onChange={changePassword}
-                        required
-                        autoComplete="on"
-                    />
-                </div>
-                <button
-                    type="submit"
-                    className="btn btn-primary d-block mx-auto"
-                    disabled={loading}
-                >
-                    Submit
-                </button>
-            </form>
+  return (
+    <div
+      className="d-flex justify-content-center align-items-center"
+      style={{ minHeight: "100vh" }}
+    >
+      <Link
+        to="/"
+        className="btn btn-secondary position-fixed top-0 start-0 m-2"
+      >
+        Home
+      </Link>
+      <form onSubmit={handleSubmit} className="p-4 bg-dark text-light rounded">
+        <h2 className="text-center mb-3">Login</h2>
+        {(errorName || errorPassword || error) && (
+          <div className="alert alert-danger p-2 mb-3" role="alert">
+            {errorName
+              ? "Make sure you entered the username correct"
+              : "Make sure you entered the password correct"}
+          </div>
+        )}
+        <div className="mb-3">
+          <label htmlFor="username" className="form-label mb-1">
+            Username
+          </label>
+          <input
+            type="text"
+            className="form-control"
+            name="username"
+            id="username"
+            placeholder="Username"
+            value={name}
+            onChange={changeName}
+            required
+            autoFocus
+          />
         </div>
-    );
+        <div className="mb-3">
+          <label htmlFor="password" className="form-label mb-1">
+            Password
+          </label>
+          <input
+            type="password"
+            className="form-control"
+            name="password"
+            id="password"
+            placeholder="Password"
+            value={password}
+            onChange={changePassword}
+            required
+            autoComplete="on"
+          />
+        </div>
+        <button
+          type="submit"
+          className="btn btn-primary d-block mx-auto"
+          disabled={loading}
+        >
+          Submit
+        </button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
