3fd3513dd722509cb271a81c951d91626ff24c3c
Mohamed Ahmed <mohamed.sayed001@eng-st.cu.edu.eg>
2022-12-31 18:59:20 +0200

    fix flow

--- client-app/src/pages/MgmtDashboard.jsx
+++ client-app/src/pages/MgmtDashboard.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable react-hooks/exhaustive-deps */
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useEffect, useState } from "react";
 import { faEye, faEdit, faList } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
--- client-app/src/routes.js
+++ client-app/src/routes.js
@@ -1,9 +1,9 @@
-import { Switch, Route } from "react-router-dom";
+import { Switch, Route, Redirect } from "react-router-dom";
 import AdminRoute from "./Auth/adminRoute";
 import ManagerRoute from "./Auth/managerRoute";
 import FanRoute from "./Auth/fanRoute";
 import Dashboard from "./pages/admin/Dashboard";
-// import Signup from "./pages/admin/Signup";
+import AdminSignup from "./pages/admin/Signup";
 import Signup from "./pages/Signup";
 import Login from "./pages/Login";
 import MgmtDashboard from "./pages/MgmtDashboard";
@@ -15,11 +15,12 @@ function Routes() {
         <Route path="/admin/dashboard" component={Dashboard} />
         <Route path="/login" component={Login} />
         <Route path="/signup" component={Signup} />
-        <AdminRoute path="/admin/signup" component={Signup} />
-        <AdminRoute path="/admin/login" component={Login} />
+        <Route path="/admin/signup" component={AdminSignup} />
+        <Route path="/admin/login" component={Login} />
         <AdminRoute path="/manager/dashboard" component={MgmtDashboard} />
-        <Route path="/signup" component={Signup} />
+        {/* <Route path="/signup" component={Signup} /> */}
         <Route path="/fan" component={FanHome} />
+        <Route path="*" component={() => Redirect("/login")} />
       </Switch>
     </>
   );
