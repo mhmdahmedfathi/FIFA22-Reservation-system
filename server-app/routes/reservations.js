3fc77c82cf37a951eac19d7e6ec1b35b4d026507
yousif-ahmed <yousif.lasheen60@gmail.com>
2022-12-31 06:56:34 +0200

    feat : adding reservation

--- server-app/routes/reservations.js
+++ server-app/routes/reservations.js
@@ -1,2 +1,86 @@
 const router = require("express").Router();
+const reservation = require("../models/Reservation");
+const Roles = require("../helpers/roles.js");
+const { authorize } = require('../middleWare/authorize');
+const match = require("../models/Match");
 
+router.get("/:id" , authorize([Roles.Manager]), (req, res) => {
+  reservation.findAll({
+    where: {
+      MatchId: req.params.id
+    }
+  }).then((reservation) => {
+    res.json(reservation);
+  }).catch((err) => {
+    res.status(500).json({ error: err });
+  });
+}
+);
+
+router.post("/", authorize([Roles.Fan]), (req, res) => {
+  // check wether the set is already reserved
+  reservation.findOne({
+    where: {
+      setNumber: req.body.setNumber,
+      MatchId: req.body.matchId
+    }
+  }).then((reservation_val) => {
+    if (reservation_val) {
+      return res.status(400).json({ error: "Set is already reserved" });
+    }
+    reservation.create({
+      date: Date.now(),
+      setNumber: req.body.setNumber,
+      MatchId: req.body.matchId,
+      UserId: req.user.id
+    }).then((reservation) => {
+      res.json(reservation);
+    })
+  }
+
+  );
+});
+
+router.delete("/:id", authorize([Roles.Fan]), (req, res) => {
+  // check the time of the match
+
+  const threeDaysAfterToday = new Date();
+  threeDaysAfterToday.setDate(threeDaysAfterToday.getDate() + 3);
+
+  // check wether the user is the owner of the reservation
+  reservation.findOne({
+    where: {
+      id: req.params.id
+    },
+    include: [{
+      model: match,
+      attributes: ['date']
+    }]
+  }).then((reservation_val) => {
+    if (!reservation_val) {
+      return res.status(404).json({ error: "Reservation not found" });
+    }
+    if (reservation_val.UserId != req.user.id) {
+      return res.status(400).json({ error: "You are not the owner of this reservation" });
+    }
+    if (reservation_val.Match.date < threeDaysAfterToday) {
+      return res.status(400).json({ error: "you can't cancel the ticket" });
+    }
+    reservation.destroy({
+      where: {
+        id: req.params.id
+      }
+    }).then((reservation) => {
+      res.json(reservation);
+    }
+    ).catch((err) => {
+      res.status(500).json({ error: err });
+    }
+    );
+  });
+
+});
+
+
+
+module.exports = router;
